f2113fa82516f3786413d158f20b99ad450a6a5d
Vega <Vegalol@users.noreply.github.com>
2015-02-17 13:20:11 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+define('COCKPIT_ADMIN', 1);
+
+// урл настройка
+if (!isset($_SERVER['COCKPIT_URL_REWRITE'])) {
+    $_SERVER['COCKPIT_URL_REWRITE'] = 'Off';
+}
+
+// стандарт таймзона
+date_default_timezone_set('UTC');
+
+// handle вебсервер
+if (PHP_SAPI == 'cli-server' && is_file(__DIR__.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// bootstrap
+require(__DIR__.'/bootstrap.php');
+
+// error pages
+$cockpit->on("after", function() {
+
+    switch ($this->response->status) {
+        case 500:
+
+            if ($this['debug']) {
+
+                if ($this->req_is('ajax')) {
+                    $this->response->body = json_encode(['error' => json_decode($this->response->body, true)]);
+                } else {
+                    $this->response->body = $this->render("cockpit:views/errors/500-debug.php", ['error' => json_decode($this->response->body, true)]);
+                }
+
+            } else {
+
+                if ($this->req_is('ajax')) {
+                    $this->response->body = '{"error": "500", "message": "system error"}';
+                } else {
+                    $this->response->body = $this->view("cockpit:views/errors/500.php");
+                }
+            }
+
+            break;
+
+        case 404:
+
+            if ($this->req_is('ajax')) {
+                $this->response->body = '{"error": "404", "message":"File not found"}';
+            } else {
+                $this->response->body = $this->view("cockpit:views/errors/404.php");
+            }
+            break;
+    }
+});
+
+//backend
+$cockpit->set('route', COCKPIT_ADMIN_ROUTE)->trigger("admin.init")->run();
+
+
+
